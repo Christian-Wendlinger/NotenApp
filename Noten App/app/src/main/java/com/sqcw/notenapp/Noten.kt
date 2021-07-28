fcc699bc087401ce9aa1151553c66188fe88398f
Christian Wendlinger <christian.wendlinger@uni-ulm.de>
2021-07-28 16:42:15 +0200

    - created Project - created Header section in Noten Screen - adjusted Android Manifest, Themes and Settings - changed Launcher Icon

---
+++ Noten App/app/src/main/java/com/sqcw/notenapp/Noten.kt
@@ -0,0 +1,11 @@
+package com.sqcw.notenapp
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+
+class Noten : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_noten)
+    }
+}
